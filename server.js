0423d89788128849ca599b2e76640a8d44b16d7f
Joel Douglass <joel.douglass@velir.com>
2015-05-22 09:36:16 -0400

    Reformatting

--- server.js
+++ server.js
@@ -1,27 +1,27 @@
 var webshot = require('webshot'),
-		http 		= require('http'),
-		fs 			= require('fs'),
-		url 		= require('url');
+	http = require('http'),
+	fs = require('fs'),
+	url = require('url');
 
 http.createServer(function (req, res) {
 
-  var options = url.parse(req.url, true).query;
+	var options = url.parse(req.url, true).query;
 
-  var targetUrl = decodeURI(options.url);
-  var shotWidth = +options.width;
-  var shotHeight = +options.height;
+	var targetUrl = decodeURI(options.url);
+	var shotWidth = +options.width;
+	var shotHeight = +options.height;
 
-  var options = {
-    shotSize: {
-      width: shotWidth || 'all',
-      height: shotHeight || 'all'
-    },
-    defaultWhiteBackground: true,
+	var options = {
+		shotSize: {
+			width: shotWidth || 'all',
+			height: shotHeight || 'all'
+		},
+		defaultWhiteBackground: true,
 		takeShotOnCallback: options.wait
-  };
+	};
 
-  webshot(targetUrl, options, function(err, renderStream) {
-    renderStream.pipe(res);
-  });
+	webshot(targetUrl, options, function (err, renderStream) {
+		renderStream.pipe(res);
+	});
 
 }).listen(process.env.PORT || 9000);

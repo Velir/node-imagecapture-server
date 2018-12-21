040473f43e75405da9c6193cae911c4dc3d04d63
Chris Eng <chris.eng@velir.com>
2018-12-21 14:49:35 -0500

    Handle POST requests (intended for use when URLs are so long that a PathTooLongException would occur with a GET request)

--- server.js
+++ server.js
@@ -4,32 +4,79 @@ var webshot = require('webshot'),
 	url = require('url');
 
 http.createServer(function (req, res) {
+	if(req.method == 'POST') {
+		var targetUrl = '';
+		var options = {};
+		var jsonString = '';		
+        req.on('data', function (data) {
+            jsonString += data;
+        });
+        req.on('end', function () {
+			try {
+				options = JSON.parse(jsonString);
+				targetUrl = options['url'];
+		
+				var shotWidth = +options.w;
+				var shotHeight = +options.h;
+				var windowWidth = +options.ww;
+				var windowHeight = +options.wh;
+				var timeout = +options.timeout;
 
-	var options = url.parse(req.url, true).query;
+				var options = {
+					shotSize: {
+						width: shotWidth || 'all',
+						height: shotHeight || 'all'
+					},
+					windowSize: {
+						width: windowWidth || 1024,
+						// The standard default of 768 doesn't allow for screen caps less than 768 tall
+						// Making the window height 1 will allow for shorter auto height images
+						height: windowHeight || 1
+					},
+					phantomPath: 'phantomjs.exe',
+					defaultWhiteBackground: true,
+					takeShotOnCallback: options.wait,
+					timeout: timeout
+				};
 
-	var targetUrl = decodeURI(options.url);
-	var shotWidth = +options.w;
-	var shotHeight = +options.h;
-	var windowWidth = +options.ww;
-	var windowHeight = +options.wh;
+				webshot(targetUrl, options, function (err, renderStream) {
+					// if (renderStream) {
+						renderStream.pipe(res);
+					// }
+				});
+			}
+			catch(error) {
+				// res.write(error);
+			}
+        });
+	}
+	else {
+		var options = url.parse(req.url, true).query;
 
-	var options = {
-		shotSize: {
-			width: shotWidth || 'all',
-			height: shotHeight || 'all'
-		},
-		windowSize: {
-			width: windowWidth || 1024,
-			// The standard default of 768 doesn't allow for screen caps less than 768 tall
-			// Making the window height 1 will allow for shorter auto height images
-			height: windowHeight || 1
-		},
-		defaultWhiteBackground: true,
-		takeShotOnCallback: options.wait
-	};
+		var targetUrl = decodeURI(options.url);
+		var shotWidth = +options.w;
+		var shotHeight = +options.h;
+		var windowWidth = +options.ww;
+		var windowHeight = +options.wh;
 
-	webshot(targetUrl, options, function (err, renderStream) {
-		renderStream.pipe(res);
-	});
+		var options = {
+			shotSize: {
+				width: shotWidth || 'all',
+				height: shotHeight || 'all'
+			},
+			windowSize: {
+				width: windowWidth || 1024,
+				// The standard default of 768 doesn't allow for screen caps less than 768 tall
+				// Making the window height 1 will allow for shorter auto height images
+				height: windowHeight || 1
+			},
+			phantomPath: 'phantomjs.exe',
+			defaultWhiteBackground: true,
+			takeShotOnCallback: options.wait,
+		};
 
+		webshot(targetUrl, options, function (err, renderStream) {
+			renderStream.pipe(res);
+		});
+	}
 }).listen(process.env.PORT || 9000);
